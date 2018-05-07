7a1a19ff03a9f40431a52d1a19c0469d5dd38da4
Victor Yamchinov <kodiers@gmail.com>
2018-05-08 02:06:05 +0300

    Initial Commit

---
+++ breakpoint-app/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  breakpoint-app
+//
+//  Created by Viktor Yamchinov on 08/05/2018.
+//  Copyright © 2018 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ breakpoint-app/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  breakpoint-app
+//
+//  Created by Viktor Yamchinov on 08/05/2018.
+//  Copyright © 2018 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
