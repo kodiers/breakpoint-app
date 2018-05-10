6c6a079dfd0545149527537f93fa4586736de286
Victor Yamchinov <kodiers@gmail.com>
2018-05-10 03:25:24 +0300

    added groupsvc/feedvc/aythservice

--- breakpoint-app/Controller/FeedVC.swift
+++ breakpoint-app/Controller/FeedVC.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  FeedVC.swift
 //  breakpoint-app
 //
 //  Created by Viktor Yamchinov on 08/05/2018.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class FeedVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- breakpoint-app/Controller/GroupsVC.swift
+++ breakpoint-app/Controller/GroupsVC.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  GroupsVC.swift
 //  breakpoint-app
 //
 //  Created by Viktor Yamchinov on 08/05/2018.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class GroupsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
